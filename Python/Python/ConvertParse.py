c839537acc3cc42f265ba3bd9cd97350717e5d6f
kb6737 <kb6737@mun.ca>
2017-02-24 16:36:28 -0330

    Add files via upload

---
+++ Python/Python/ConvertParse.py
@@ -0,0 +1,100 @@
+def convert_parse_labs(name,course):
+	labs = course['lab hours'] if 'lab hours' in course else 0
+	if labs == 'at least 9 hours per semester':
+		labs = 9
+	if labs == 'at least 12 hours per semester':
+		labs = 12
+	if labs == 'at least ten 2-hour sessions per semester':
+		labs = 20
+	if labs == 'at least five 3-hour sessions per semester':
+		labs = 15
+	if labs == 'at least three 1-hour sessions per semester':
+		labs = 3
+	if labs == 'at least one 4-hour session per semester':
+		labs = 4
+	if labs == 'at least four 2-hour sessions per semester':
+		labs = 8
+	if labs == 'at least eight 1-hour sessions per semester':
+		labs = 8
+	if labs == 'at least four 3-hour sessions per semester':
+		labs = 12
+	if labs == 'at least ten 3-hour sessions per semester':
+		labs = 30
+	if labs == 'at least three 3-hour sessions per semester':
+		labs = 9
+	if labs == 'at least six 3-hour laboratory sessions per semester':
+		labs = 18
+	if labs == 'at least four 3-hour sessions per term':
+		labs = 12
+	if labs == 'at least nine 2-hour sessions per semester':
+		labs = 18
+	if labs == 'at least eight 3-hour sessions per semester':
+		labs = 24
+	if labs == 'at least eight 2-hour sessions per semester':
+		labs = 16
+	if labs == 'at least one 3-hour session per semester':
+		labs = 3
+	if labs == 'at least 6 hours per semester':
+		labs = 6
+	if labs == 'at least two 2-hour sessions per semester':
+		labs = 4
+	if labs == 'at least six 3-hour sessions per semester':
+		labs = 18
+	if labs == 'at least four 1-hour sessions per semester':
+		labs = 4
+	if labs == 'scheduled as required':
+		labs = 3
+	if labs == 'at least four 3-hour sessions per semester.':
+		labs = 12
+	if labs == 'at least five 1-hour sessions per semester':
+		labs = 5
+	if labs == 'at least ten 3-hour lab sessions per semester':
+		labs = 10
+	if labs == 'at least 20 hours per semester':
+		labs = 20
+	if labs == 'at least nine 3-hour sessions per semester':
+		labs = 27
+	if labs == 'at least three 1.5-hour sessions per semester':
+		labs = 4.5
+	if labs == 'at least nine 3-hour laboratory sessions per semester':
+		labs = 27
+	if labs == 'five 3-hour sessions per semester':
+		labs = 15
+	if labs == 'at least seven 2-hour sessions per semester':
+		labs = 14
+	if labs == 'at least six 2-hour sessions per semester':
+		labs = 6
+	if labs == 'at least 4 three-hour sessions per semester':
+		labs = 12
+	labs1=str(labs)
+	return labs1
+
+def convert_parse_credit_hours(name,course):
+	credit_hours = course['credit-hours'] if 'credit-hours' in course else 0
+	credit_hours1=str(credit_hours)
+	return credit_hours1
+
+def convert_parse_lecture_hours(name, course):
+	lecture_hours = course['lecture hours'] if 'lecture hours' in course else 0
+	if lecture_hours == 'at least 10 lecture hours per semester':
+		lecture_hours = 10
+	if lecture_hours == 'at least 15 lecture hours per semester':
+		lecture_hours = 15
+	if lecture_hours == 'scheduled as required':
+		lecture_hours = 3
+	lecture_hours1=str(lecture_hours)
+	return lecture_hours1
+def convert_parse_title(name, course):
+	title = course['title'] if 'title' in course else None
+	return title
+def convert_parse_comments(name, course):
+	comments = course['description'] if 'description' in course else None
+	return comments
+def convert_parse_course_id(name, course):
+	course_id = course['number'] if 'number' in course else None
+	course_id1=str(course_id)
+	return course_id1
+
+def passer():
+	passer.has_been_called = True
+	pass
---
+++ Python/Python/graphcourses.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+
+import argparse
+import calendar as cal
+import collections
+import sys
+import csv
+from Class import *
+from ConvertParse import *
+args = argparse.ArgumentParser()
+args.add_argument('filename', nargs = '+')
+args.add_argument('--format', choices = [ 'calendar', 'dot' ], default = 'dot')
+args.add_argument('-o', '--output', default = '-')
+
+args = args.parse_args()
+output = sys.stdout if args.output == '-' else open(args.output, 'w')
+
+
+courses = collections.defaultdict(dict)
+
+
+for filename in args.filename:
+	f = open(filename)
+	html = filename.endswith('.htm') or filename.endswith('.html')
+	parse = cal.parseHTML if html else cal.parseText
+
+	for (name,c) in parse(open(filename), prefix = 'ENGI').items():
+		courses[name].update(c)
+
+	for course in courses.values():
+		if course['name'].startswith('ENGI '):
+			num = str(course['number'])
+			term = int(num[0])
+			department = int(num[1])
+
+dictyear={'2008':courses}
+
+with open('dict.csv', 'wb') as csv_file:
+	writer = csv.writer(csv_file)
+	for key, value in courses.items():
+		writer.writerow([key, value])
+
+
+
+for name, course in courses.items():
+	cc = Course1()
+	g = CourseGeneration1()
+	print course.keys()
+	length=len(courses)
+	labs = convert_parse_labs(name, course)
+	credit_hours = convert_parse_credit_hours(name, course)
+	lecture_hours = convert_parse_lecture_hours(name, course)
+	title = convert_parse_title(name, course)
+	comments = convert_parse_comments(name, course)
+	course_num = convert_parse_course_id(name, course)
+	try:
+		cc.create(subject="ENGI", course_num=course_num)
+		b = Course1.select().order_by(Course1.course_id.desc()).get()
+		g.create(labs=labs, credit_hours=credit_hours, lecture_hours=lecture_hours, title=title, comments=comments,
+			 course_id=b)
+	except:
+		pass
+	if course['number']=='8801-8805 (Excluding 8804)':
+		try:
+			cc.create(subject='ENGI', course_num=course_num)
+			b = Course1.select().order_by(Course1.course_id.desc()).get()
+			g.create(labs=labs, credit_hours=credit_hours, lecture_hours=lecture_hours, title=title, comments=comments,
+					 course_id=b)
+		except:
+			print "pass"
+			pass
+	else:
+		length2 = cc.select().count()
+		for x in cc.select():
+			length2=cc.select().count()
+			print length2
+			c=x.course_id
+			a=x.course_num
+			b=x.subject
+			if course_num==a :
+				for y in g.select():
+					d=(y.course_id)
+					e=(y.comments)
+					f=str(y.title)
+					h=str(y.lecture_hours)
+					i=str(y.credit_hours)
+					j=str(y.labs)
+					if c==d.course_id:
+						if   e!=comments or f!=title or (y.lecture_hours)!=lecture_hours or i!=credit_hours or j!=labs:
+							g.create(labs=labs, credit_hours=credit_hours, lecture_hours=lecture_hours, title=title,
+								 comments=comments, course_id=d.course_id)
