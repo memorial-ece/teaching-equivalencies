f3937f767cca3975b86ff833a42e750ed45676f9
kb6737 <kb6737@mun.ca>
2017-03-01 17:59:28 +0000

    Add files via upload

--- graph-courses.py
+++ graph-courses.py
@@ -5,8 +5,8 @@ import calendar as cal
 import collections
 import sys
 import csv
-from pump_a_rum import *
 import re
+from ConvertParse import *
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
@@ -28,13 +28,18 @@ count=(-1)
 p1 = re.compile(r"(\d+\b)(?!.*\1\b)")
 p2 = re.compile(r"((filen1[count])+......\w)")
 filen1 = (p1.findall(str(args.filename)))
-for x in filen1:
+for x1 in filen1:
+	x1=int(x1)
+	x2=x1
+	if x1!=2008:
+		x2=x1-1
+	print x2
+	# print x
 	for filename in args.filename:
 		p1 = re.compile(r"(\d+\b)(?!.*\1\b)")
 		filen2 = ''.join(p1.findall(str(filename)))
-		# print filen2
-		# print x
-		if filen2==x:
+		filen2= int(filen2)
+		if filen2==x1:
 			f = open(filename)
 			html = filename.endswith('.htm') or filename.endswith('.html')
 			parse = cal.parseHTML if html else cal.parseText
@@ -44,7 +49,10 @@ for x in filen1:
 				# print b
 				check=obscure_refference(name,c)
 				dict_course[crn].update(obscure_refference(name, c))
-				dict_year[x].update(dict_course)
-				print x
+				dict_year[x1].update(dict_course)
+				# print dict_year[x1][crn]["Title"]
+				print crn
+				print dict_year[x2][crn]["Title"]
+				print dict_year[x1][crn]["Title"]
 
-print dict_year['2009'][1]['Title']
+# print dict_year['2009'][1]['Title']
