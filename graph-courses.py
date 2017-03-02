8275e14c490ba02d2b07faa171904c76514793d4
kb6737 <kb6737@mun.ca>
2017-03-02 13:44:32 +0000

    Add files via upload

--- graph-courses.py
+++ graph-courses.py
@@ -6,6 +6,7 @@ import collections
 import sys
 import csv
 import re
+from operator import *
 from ConvertParse import *
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -21,20 +22,20 @@ output = sys.stdout if args.output == '-' else open(args.output, 'w')
 
 courses = collections.defaultdict(dict)
 dict_course= collections.defaultdict(dict)
+dict_rando= collections.defaultdict(dict)
 dict_year=collections.defaultdict(dict)
 dict_gen=collections.defaultdict(dict)
-crn=0
+dict_view_gen=collections.defaultdict(dict)
+
 count=(-1)
 p1 = re.compile(r"(\d+\b)(?!.*\1\b)")
 p2 = re.compile(r"((filen1[count])+......\w)")
 filen1 = (p1.findall(str(args.filename)))
 for x1 in filen1:
 	x1=int(x1)
-	x2=x1
 	if x1!=2008:
 		x2=x1-1
-	print x2
-	# print x
+
 	for filename in args.filename:
 		p1 = re.compile(r"(\d+\b)(?!.*\1\b)")
 		filen2 = ''.join(p1.findall(str(filename)))
@@ -44,15 +45,31 @@ for x1 in filen1:
 			html = filename.endswith('.htm') or filename.endswith('.html')
 			parse = cal.parseHTML if html else cal.parseText
 			for (name,c) in parse(open(filename), prefix='ENGI').items():
-				crn += 1
-				b = str(crn)
-				# print b
+				code=convert_parse_course_id(name,c)
 				check=obscure_refference(name,c)
-				dict_course[crn].update(obscure_refference(name, c))
-				dict_year[x1].update(dict_course)
-				# print dict_year[x1][crn]["Title"]
-				print crn
-				print dict_year[x2][crn]["Title"]
-				print dict_year[x1][crn]["Title"]
+				dict_course[code].update(check)
+				# print code
+				# print dict_year[x1]!=dict_year[x2]
 
-# print dict_year['2009'][1]['Title']
+					# print dict_year[x1]
+					# print dict_year[x2]
+				# print dict_course
+	dict_year[x1].update(dict_course)
+
+for year,course in dict_year.items():
+	span=year
+	list_keys_year=dict_year[year].keys()
+	if dict_gen==collections.defaultdict(dict):
+		for i in range(len(list_keys_year)):
+			dict_gen[list_keys_year[i]]=course[list_keys_year[i]]
+	list_gen=dict_gen.keys()
+	for c in list_keys_year:
+		if c not in list_gen:
+			dict_gen[c]=course[c]
+		if c in list_gen:
+			if dict_gen[c]!=(course[c]):
+				dict_gen[c]=course[c]
+	for c in list_gen:
+		if c not in list_keys_year:
+			dict_gen.pop(c)
+	dict_view_gen[year]=dict_gen
