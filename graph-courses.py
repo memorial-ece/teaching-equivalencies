45d2beea294c055f7e96d4fc0d559de7517af9df
kb6737 <kb6737@mun.ca>
2017-03-03 14:44:16 +0000

    Add files via upload

--- graph-courses.py
+++ graph-courses.py
@@ -6,7 +6,7 @@ import collections
 import sys
 import csv
 import re
-from operator import *
+import operator
 from ConvertParse import *
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -34,95 +34,60 @@ for filename in args.filename:
 		codex=convert_parse_course_id(name,c)
 		course=obscure_refference(name,c)
 		courses[filen2].append(course)
-		# print courses.items()
 
 for year, cour in courses.items():
 	dict_year = collections.defaultdict(dict)
 	for course in cour:
 		a=courses[year].index(course)
-		code=course['name']
+		code=course['Name']
 		dict_year[year]=courses[year][a]
-		# print dict_year.items()
-		# print dict_year
-		# print courses[year][a]
-		# print dict_year.items()
-		# print code
 		list_course[code][year] = course
-		# print list_course.items()
 
-# for x in list_course:
-
-print list_course
-# print list_course.values()
-
-
-		# if code!=
-# print list_course.keys()
-
-
-
-
-
-
-
-
-
-
-
-
-# for name,ye_de in list_course.items():
-# 	print list_course
-	# year=sorted(ye_de.items())
-	# decs=list_course[name].values()
-	# print decs
-	# a=-1
-	# for i in range(len(decs)):
-	# 	a=i+1
-	# 	try:
-	# 		if decs[i]==decs[a]:
-	# 			print decs[i]
-	# 			dict_gen[i]=decs[i]
-	# 			print dict_gen
-	#
-	# 	except:
-	# 		pass
-
-
-
-
-
-
-
-
-
-
-		# compare=decs[i]
-		# try:
-		# 	i+=1
-		# 	if compare==decs[i]:
-		# 		print name
-		# 		print decs
-		# except:
-		# 	print 'll'
-	# print year_and_description
-	# for i in range(len(compare)):
-	# 	# print 'lol'
-	# 	print
-	# if dict_gen==collections.defaultdict(dict):
-	# 	for i in range(len(compare)):
-	# 		if dict_gen.values()!=compare:
-	# 			pass
-	# 		dict_gen[name]=compare[i]
-	#
-	# list_gen=dict_gen.keys()
-	# print list_gen
-	# for c in list_keys_year:
-	# 	if c not in list_gen:
-	# 		dict_gen[c]=course[c]
-	# 	if c in list_gen:
-	# 		if dict_gen[c]!=(course[c]):
-	# 			dict_gen[c]=course[c]
-	# for c in list_gen:
-	# 	if c not in list_keys_year:
-	# 		dict_gen.pop(c)
-	# dict_view_gen[year]=dict_gen
+# print list_course
+a=0
+for code, details in list_course.items():
+	sorted_det= sorted(details.items(), key=operator.itemgetter(0))
+	year2=None
+	info2=None
+	peewee_check=None
+	try:
+		peewee_check = CourseGeneration1.select().join(Course1).where(Course1.course_num == code).order_by(CourseGeneration1.year_of_valid_generation.desc()).get()
+	except:
+		pass
+	for keyyear in sorted_det:
+		year1,info1=keyyear[0],keyyear[1]
+		if peewee_check!=None:
+			print 'data in table'
+			pc=peewee_check
+			if pc.labs!=info1['Labs'] or pc.credit_hours!=info1['Credit Hours'] or pc.lecture_hours!=info1['Lecture Hours'] or pc.title!=info1['Title'] or pc.comments!=info1['Description'] or pc.other_info!=info1['Other info'] or pc.old_course_id!=info1['OldCourseCode'] and pc.year_of_valid_generation==year1:
+				print pc.old_course_id!=info1['OldCourseCode']
+				print pc.course_gen_id
+		else:
+			if year1>year2:
+				if info1!=info2:
+					info2=info1
+					year2=year1
+					print 'clean'
+					try:
+						Course1.create(subject="ENGI", course_num=code)
+						late_update = Course1.select().where(Course1.course_num==code).get()
+						CourseGeneration1.create(labs=info1['Labs'], credit_hours=info1['Credit Hours'],
+												 lecture_hours=info1['Lecture Hours'],
+												 title=info1['Title'], comments=info1['Description'],
+												 course_id=late_update, other_info=info1['Other info'],
+												 old_course_id=info1['OldCourseCode'], year_of_valid_generation=year1,year_valid_to=year1)
+					except:
+						late_update = Course1.select().where(Course1.course_num==code).get()
+						CourseGeneration1.create(labs=info1['Labs'],credit_hours=info1['Credit Hours'],
+											 lecture_hours=info1['Lecture Hours'],
+											 title=info1['Title'],comments=info1['Description'],
+											 course_id=late_update,other_info=info1['Other info'],
+											 old_course_id=info1['OldCourseCode'],year_of_valid_generation=year1,year_valid_to=year1)
+				else:
+					print'update'
+					update=CourseGeneration1.update(year_valid_to=year1).where(CourseGeneration1.course_gen_id==CourseGeneration1.select().order_by(CourseGeneration1.course_gen_id.desc()).get())
+					update.execute()
+			else:
+				year2=year1
+				info2=info1
+				print 'error'
