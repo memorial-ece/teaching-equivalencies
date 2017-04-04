5338bccd6631d9ff702ffcd63383b2b1e33be85c
kb6737 <kb6737@mun.ca>
2017-04-04 01:27:54 -0230

    git through pycharm

--- Startup.py
+++ Startup.py
@@ -15,20 +15,20 @@
 
 """Usage:
 	Startup.py execute (FUNCTION) [ARGS]
+	Startup.py intake (<FILE>)...
 	Startup.py populate (<FILE>)...
 	Startup.py -h --help
 
 Options:
-	(docustomexport)[Table] Any table form class.py
-	(docustomimport)[Table]	Any table form class.py
+	execute (docustomexport)[Table] Any table form class.py
+	execute (docustomimport)[Table]	Any table form class.py
 	populate [file location] gen all data from offerings folder program will crash if the year exceeds 2030
-	(peeweetable)[DropType] droptypes are 'DropReCreate' 'Drop' 'Create'
-	(test1) prints teststring file
-	(test2) prints teststring file
-	(test3) prints a timer
+	execute (peeweetable)[DropType] droptypes are 'DropReCreate' 'Drop' 'Create' shortcuts 'pt', 'drc' , 'd' , 'c'
+
 """
 import docopt
 from Core import *
+from import_courses import *
 
 
 if __name__ == '__main__':
@@ -37,17 +37,33 @@ if __name__ == '__main__':
 		function = (arguments['FUNCTION'])
 		arg = str(arguments['ARGS'])
 		filename = arguments['<FILE>']
+		if function=='pt':
+			if arg=='d':
+				arg='Drop'
+			if arg=='c':
+				arg='Create'
+			if arg=='drc':
+				arg='DropReCreate'
+			peeweetable(arg)
 		if function == 'docustomexport':
 			docustomexport(arg)
 		if function == 'docustomimport':
 			docustomimport(arg)
 		if function == 'peeweetable':
+			if arg=='d':
+				arg='Drop'
+			if arg=='c':
+				arg='Create'
+			if arg=='drc':
+				arg='DropReCreate'
 			peeweetable(arg)
 		# if function == 'test1':
 		# if function == 'test2':
 		# if function == 'test3':
-		if filename != []:
+		if arguments['populate']==True:
 			populate(filename)
+		if arguments['intake']==True:
+			import_courses(filename)
 
 	except docopt.DocoptExit as e:
 		print e.message
