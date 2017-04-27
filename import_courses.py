51c9629169e55f5169e4a7e7c8a29e8f7d7134dd
Jonathan Anderson <jonathan.anderson@ieee.org>
2017-04-26 22:57:05 -0230

    Simplify course importing.

--- import_courses.py
+++ import_courses.py
@@ -1,118 +1,92 @@
-#  Copyright 2017 Keegan Joseph Brophy
+# Copyright 2017 Jonathan Anderson
+# Copyright 2017 Keegan Joseph Brophy
 #
-#    Licensed under the Apache License, Version 2.0 (the "License");
-#    you may not use this file except in compliance with the License.
-#    You may obtain a copy of the License at
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-#        http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS,
-#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#    See the License for the specific language governing permissions and
-#    limitations under the License.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import collections
+import reader as calendar
+
+from ConvertParse import sanitize_course
+
 from Core import *
 import operator
-import reader as cal
 from db import *
-from ConvertParse import *
 
 
-def import_courses(filename_import):
-    courses = collections.defaultdict(list)
-    for filename in filename_import:
-        parser1 = re.compile(r"\d+\D\w+$")
-        filename_year_subsection = (''.join(parser1.findall(str(filename))))
-        filename_year_subsection = filename_year_subsection.strip("'.html'")
-        parse = cal.parseHTML
-        for (name, c) in parse(open(filename)).items():
+def import_courses(filenames):
+    file_count = len(filenames)
+    print('Parsing %d calendar files...' % file_count)
+
+    # Parse each (annual) calendar's description of every course.
+    courses_by_year = collections.defaultdict(list)
+
+    for (i, filename) in enumerate(filenames):
+        progress(i, file_count)
+
+        year = int(re.findall(r'\d{4}', filename)[-1])
+
+        for (name, c) in calendar.parseHTML(open(filename)).items():
             course = sanitize_course(name, c)
-            courses[filename_year_subsection].append(course)
-    list_course = collections.defaultdict(dict)
-    for year, cour in courses.items():
-        for course in cour:
+            courses_by_year[year].append(course)
+
+    progress(1, 1)
+    print('\n')
+
+    # Arrange courses by course code (e.g., ENGI 200W) and then by year.
+    courses = collections.defaultdict(dict)
+    for (i, (year, year_courses)) in enumerate(courses_by_year.items()):
+        for course in year_courses:
+            subject = course['Subject']
             code = course['Name']
-            list_course[code][year] = course
-    progress_counter = 0
-    for code, details in list_course.items():
-        progress_counter += 1
-        try:
-            progress(progress_counter, len(list_course))
-        except:
-            pass
-        sorted_det = sorted(details.items(), key=operator.itemgetter(0))
-        lastyear = None
-        info2 = None
-        peewee_check = None
-        for keyyear in sorted_det:
-            thisyear, info1 = keyyear[0], keyyear[1]
-            var1 = re.compile(r"....")
-            thisyear = str(var1.findall(str(thisyear)))
-            thisyear = thisyear.strip("'[]")
-            try:
-                peewee_check = CourseGeneration.select()\
-                    .join(Course).where(Course.code == code, CourseGeneration.start_year == thisyear).get()
-            except:
-                pass
-            if peewee_check is not None:
-                print 'if you see this no data should update'
-                info2 = info1
-                lastyear = thisyear
-                pc = peewee_check
-                if pc.labs != info1['Labs'] or pc.credit_hours != info1['Credit Hours'] \
-                        or pc.lecture_hours != info1['Lecture Hours'] or pc.title != info1['Title'] \
-                        or pc.comments != info1['Description'] or pc.other_info != info1['Other info'] \
-                        or pc.previous_course_id != info1['PreviousCourseCode']:
-                    if pc.start_year == thisyear:
-                        late_update = Course.select().where(Course.code == code).get()
-                        CourseGeneration.update(labs=info1['Labs'], credit_hours=info1['Credit Hours'],
-                                                lecture_hours=info1['Lecture Hours'],
-                                                title=info1['Title'], comments=info1['Description'],
-                                                course=late_update, other_info=info1['Other info'],
-                                                previous_course=info1['PreviousCourseCode'], start_year=thisyear,
-                                                end_year=thisyear,reviewed=False) \
-                            .where(CourseGeneration.id == pc.id)
-                elif info1 != info2 and thisyear > pc.end_year:
-                    info2 = info1
-                    lastyear = thisyear
-                    late_update = Course.select().where(Course.code == code).get()
-                    CourseGeneration.create(labs=info1['Labs'], credit_hours=info1['Credit Hours'],
-                                            lecture_hours=info1['Lecture Hours'],
-                                            title=info1['Title'], comments=info1['Description'],
-                                            course=late_update, other_info=info1['Other info'],
-                                            previous_course=info1['PreviousCourseCode'], start_year=thisyear,
-                                            end_year=thisyear,reviewed=False)
-                elif info1 == info2 and thisyear > pc.end_year:
-                    update = CourseGeneration.update(end_year=thisyear).where(
-                        CourseGeneration.id == pc.id)
-                    update.execute()
+            courses[(subject, code)][year] = course
+
+    # For each course, identify homogeneous "generations".
+    #
+    # For example, if a course was introduced in 2010 and modified in 2013,
+    # the calendar descriptions in 2010, 2011 and 2012 will be identical but
+    # the description in 2013 will be different. We need to effectively
+    # reverse-engineer the calendar change process, resulting in a
+    # 2010-2012 generation and a 2013-??? generation.
+    course_count = len(courses)
+    print('Identifying generations of %d courses...' % course_count)
+    for (i, ((subject, code), details_by_year)) in enumerate(courses.items()):
+        progress(i, course_count)
+
+        course, created = Course.get_or_create(subject = subject, code = code)
+        gen = None
+
+        for year in sorted(details_by_year.keys()):
+            details = details_by_year[year]
+
+            # Is this the first time we've seen this course description?
+            if gen is None or gen.differs_from(details):
+                gen = CourseGeneration.create(
+                    course = course,
+                    labs = details['Labs'],
+                    credit_hours = details['Credit Hours'],
+                    lecture_hours = details['Lecture Hours'],
+                    title = details['Title'],
+                    comments = details['Description'],
+                    other_info = details['Other info'],
+                    previous_course = details['PreviousCourseCode'],
+                    start_year = year,
+                    end_year = year)
+
+            # Otherwise, it's another year of the same thing.
             else:
-                if thisyear > lastyear:
-                    if info1 != info2:
-                        info2 = info1
-                        lastyear = thisyear
-                        try:
-                            Course.get_or_create(subject=info1['Subject'], code=code,reviewed=False)
-                            late_update = Course.select().where(Course.code == code).get()
-                            CourseGeneration.create(labs=info1['Labs'], credit_hours=info1['Credit Hours'],
-                                                    lecture_hours=info1['Lecture Hours'],
-                                                    title=info1['Title'], comments=info1['Description'],
-                                                    course=late_update, other_info=info1['Other info'],
-                                                    previous_course=info1['PreviousCourseCode'], start_year=thisyear,
-                                                    end_year=thisyear,reviewed=False)
-                        except:
-                            late_update = Course.select().where(Course.code == code).get()
-                            CourseGeneration.create(labs=info1['Labs'], credit_hours=info1['Credit Hours'],
-                                                    lecture_hours=info1['Lecture Hours'], title=info1['Title'],
-                                                    comments=info1['Description'], course=late_update,
-                                                    other_info=info1['Other info'],
-                                                    previous_course=info1['PreviousCourseCode'],
-                                                    start_year=thisyear, end_year=thisyear,reviewed=False)
-                    else:
-                        update = CourseGeneration.update(end_year=thisyear).where(
-                            CourseGeneration.id == CourseGeneration.select().order_by(
-                                CourseGeneration.id.desc()).get())
-                        update.execute()
-    print
+                gen.end_year = year
+                gen.save()
+
+    progress(1, 1)
+    print('\n')
+
